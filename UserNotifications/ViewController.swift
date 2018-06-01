bb30ee1ea07c48a5f2f5f5f337271fed963df917
Anshu Vij <anshu_mac@Anshus-MacBook-Pro.local>
2018-06-02 01:51:07 +0530

    Inital status

---
+++ UserNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotifications
+//
+//  Created by Anshu Vij on 02/06/18.
+//  Copyright © 2018 Anshu Vij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
